810825524364d8e4f5869377d62b84548061e571
Maximillian Arruda <dearrudam@gmail.com>
2024-02-08 21:36:29 -0300

    chore: remove testcontainer dependencies
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- src/test/java/org/redlich/beers/Database.java
+++ src/test/java/org/redlich/beers/Database.java
@@ -2,27 +2,17 @@ package org.redlich.beers;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
 
 public enum Database {
 
     INSTANCE;
 
-    private final MongoDBContainer mongodb =
-            new MongoDBContainer("mongo:latest")
-                    .withExposedPorts(27017)
-                    .waitingFor(Wait.defaultWaitStrategy());
-
-    {
-        mongodb.start();
-    }
 
     public MongoClient getMongoClient() {
-        return MongoClients.create(mongodb.getConnectionString());
+        return MongoClients.create(getConnectionString());
     }
 
     public String getConnectionString() {
-        return mongodb.getHost() + ":" + mongodb.getFirstMappedPort();
+        throw new UnsupportedOperationException("to be implemented!");
     }
 }
