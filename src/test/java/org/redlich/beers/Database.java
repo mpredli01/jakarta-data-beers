3208852f6bc09f4c07165c5c65df166e7bc57f35
Maximillian Arruda <dearrudam@gmail.com>
2024-02-08 21:49:22 -0300

    test(integration): changed to use embedded mongodb
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- src/test/java/org/redlich/beers/Database.java
+++ src/test/java/org/redlich/beers/Database.java
@@ -2,10 +2,24 @@ package org.redlich.beers;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import de.flapdoodle.embed.mongo.commands.ServerAddress;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.mongo.transitions.Mongod;
+import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess;
+import de.flapdoodle.reverse.TransitionWalker;
 
 public enum Database {
 
     INSTANCE;
+
+
+    private final TransitionWalker.ReachedState<RunningMongodProcess> mongoRunningProcess;
+
+    Database() {
+        this.mongoRunningProcess = Mongod
+                .instance()
+                .start(Version.Main.V7_0);
+    }
 
 
     public MongoClient getMongoClient() {
@@ -13,6 +27,12 @@ public enum Database {
     }
 
     public String getConnectionString() {
-        throw new UnsupportedOperationException("to be implemented!");
+        return connectionStringOf(mongoRunningProcess.current().getServerAddress());
+    }
+
+    private String connectionStringOf(ServerAddress serverAddress) {
+        return "%s:%s".formatted(
+                serverAddress.getHost(),
+                serverAddress.getPort());
     }
 }
