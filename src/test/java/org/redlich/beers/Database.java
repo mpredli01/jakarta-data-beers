375cce5bd781f4bbdc751ad927849a100f4be595
Michael P. Redlich <mike@redlich.net>
2024-02-09 13:44:29 -0500

    Max updates: 2.8.2024

--- src/test/java/org/redlich/beers/Database.java
+++ src/test/java/org/redlich/beers/Database.java
@@ -2,13 +2,17 @@ package org.redlich.beers;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
+import de.flapdoodle.embed.mongo.commands.ServerAddress;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.mongo.transitions.Mongod;
+import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess;
+import de.flapdoodle.reverse.TransitionWalker;
 
 public enum Database {
 
     INSTANCE;
 
+<<<<<<< HEAD
     /*
     private final MongoDBContainer mongodb =
             new MongoDBContainer("mongo:latest")
@@ -19,16 +23,35 @@ public enum Database {
     /*
     {
         mongodb.start();
+=======
+
+    private final TransitionWalker.ReachedState<RunningMongodProcess> mongoRunningProcess;
+
+    Database() {
+        this.mongoRunningProcess = Mongod
+                .instance()
+                .start(Version.Main.V7_0);
+>>>>>>> 3208852f6bc09f4c07165c5c65df166e7bc57f35
     }
      */
 
+<<<<<<< HEAD
     /*
+=======
+
+>>>>>>> 3208852f6bc09f4c07165c5c65df166e7bc57f35
     public MongoClient getMongoClient() {
-        return MongoClients.create(mongodb.getConnectionString());
+        return MongoClients.create(getConnectionString());
     }
 
     public String getConnectionString() {
-        return mongodb.getHost() + ":" + mongodb.getFirstMappedPort();
+        return connectionStringOf(mongoRunningProcess.current().getServerAddress());
+    }
+
+    private String connectionStringOf(ServerAddress serverAddress) {
+        return "%s:%s".formatted(
+                serverAddress.getHost(),
+                serverAddress.getPort());
     }
      */
 }
