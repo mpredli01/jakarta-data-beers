08b260866f764acc7b6b290cfbbb7cff4f110cb4
Michael P. Redlich <mike@redlich.net>
2024-02-26 18:32:09 -0500

    Initial addition of JavaDocs

--- src/main/java/org/redlich/beers/Beer.java
+++ src/main/java/org/redlich/beers/Beer.java
@@ -8,54 +8,78 @@ import jakarta.validation.constraints.NotNull;
 
 import java.util.Objects;
 
+/**
+ * A POJO representing the Beer collection in the MongoDB database.
+ */
 @Entity
 public class Beer {
+    /**
+     *
+     */
     @Id
     private int id;
-
+    /**
+     *
+     */
     @Column
     @NotBlank
     private String name;
 
+    /**
+     *
+     */
     @Column
     @NotNull
     private BeerType type;
 
+    /**
+     *
+     */
     @Column("brewer_id")
     private int brewerId;
 
+    /**
+     *
+     */
     @Column
     private double abv;
 
+    /**
+     *
+     */
     public Beer() {
         id = 0;
         name = "{ beer name }";
         type = BeerType.ALE;
         brewerId = 0;
         abv = 10.0;
-    }
+        }
 
+    /**
+     *
+     * @param id
+     * @param name
+     * @param type
+     * @param brewerId
+     * @param abv
+     */
     private Beer(int id, String name, BeerType type, int brewerId, double abv) {
         this.id = id;
         this.name = name;
         this.type = type;
         this.brewerId = brewerId;
         this.abv = abv;
-    }
+        }
 
     /**
-     * public int getId()
-     *
-     * @return id of the Beer entity.
+     * @return id the primary key the Beer entity.
      */
     public int getId() {
         return id;
-    }
+        }
 
     /**
-     * public String name()
-     *
-     * @return the name of the beer.
+     * @return name the name of the beer.
      */
     public String getName() {
         return name;
@@ -69,19 +93,22 @@ public class Beer {
     }
 
     /**
-     * @return the value of `brewerId` from the Brewer entity.
+     * @return brewerId the value of `brewerId` from the Brewer entity.
      */
     public int getBrewerId() {
         return brewerId;
     }
 
     /**
-     * @return the value of `abv`.
+     * @return abv the value of `abv`.
      */
     public double getAbv() {
         return abv;
-    }
+        }
 
+    /**
+     * @return string a string to encapsulate all the values
+     */
     @Override
     public String toString() {
         return "Beer { " +
@@ -91,8 +118,13 @@ public class Beer {
                 ", brewer_id = '" + getBrewerId() + '\'' +
                 ", abv = '" + getAbv() + '\'' +
                 " }\n";
-    }
+        }
 
+    /**
+     *
+     * @param o the object for comparison
+     * @return boolean the result of the equals comparison
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -103,17 +135,28 @@ public class Beer {
                 && Double.compare(abv, beer.abv) == 0
                 && Objects.equals(name, beer.name)
                 && type == beer.type;
-    }
+        }
 
+    /**
+     *
+     * @return int an integer resulting from the hashCode.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(id, name, type, brewerId, abv);
-    }
+        }
 
+    /**
+     *
+     * @return BeerBuilder a new instance of the BeerBuilder.
+     */
     public static BeerBuilder builder() {
         return new BeerBuilder();
-    }
+        }
 
+    /**
+     * A builder pattern for the Beer class.
+     */
     public static class BeerBuilder {
         private int id;
         private String name;
@@ -122,35 +165,35 @@ public class Beer {
         private double abv;
 
         private BeerBuilder() {
-        }
+            }
 
         public BeerBuilder id(int id) {
             this.id = id;
             return this;
-        }
+            }
 
         public BeerBuilder name(String name) {
             this.name = name;
             return this;
-        }
+            }
 
         public BeerBuilder type(BeerType type) {
             this.type = type;
             return this;
-        }
+            }
 
         public BeerBuilder brewerId(int brewerId) {
             this.brewerId = brewerId;
             return this;
-        }
+            }
 
         public BeerBuilder abv(double abv) {
             this.abv = abv;
             return this;
-        }
+            }
 
         public Beer build() {
             return new Beer(id, name, type, brewerId, abv);
+            }
         }
     }
-}
